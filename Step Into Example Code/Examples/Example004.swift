fe75c22b1ec9d0d804ab1f7abfa9c45402fbcb46
Joseph Simpson <joe@radicalappdev.com>
2024-10-15 15:58:55 -0400

    Starting work on a tap gesture

--- Step Into Example Code/Examples/Example004.swift
+++ Step Into Example Code/Examples/Example004.swift
@@ -15,15 +15,41 @@ import RealityKit
 import RealityKitContent
 
 struct Example004: View {
+
+    @State var selected: String = ""
+
     var body: some View {
         RealityView { content in
+            // Load the scene from the Reality Kit bindle
             if let scene = try? await Entity(named: "GestureLabs", in: realityKitContentBundle) {
                 content.add(scene)
 
+                // Lower the entire scene to the bottom of the volume
                 scene.position.y = -0.4
-
             }
         }
+        .ornament(attachmentAnchor: .scene(.back), ornament: {
+            Text(selected)
+                .font(.largeTitle)
+                .padding(18)
+                .background(.stepGreen)
+                .cornerRadius(12)
+                .opacity(selected.isEmpty ? 0 : 1)
+        })
+        .gesture(tapExample)
+    }
+
+    var tapExample: some Gesture {
+        SpatialTapGesture()
+            .targetedToAnyEntity()
+            .onEnded { value in
+                if(selected == value.entity.name) {
+                    selected = ""
+                } else {
+                    selected = value.entity.name
+
+                }
+            }
     }
 }
 
