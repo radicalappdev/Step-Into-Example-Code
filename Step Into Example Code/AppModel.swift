90e1f6a553307be1483c286b10c6c5a2c43a3616
Joseph Simpson <joe@radicalappdev.com>
2024-10-15 15:06:52 -0400

    Initial Commit

---
+++ Step Into Example Code/AppModel.swift
@@ -0,0 +1,21 @@
+//
+//  AppModel.swift
+//  Step Into Example Code
+//
+//  Created by Joseph Simpson on 10/15/24.
+//
+
+import SwiftUI
+
+/// Maintains app-wide state
+@MainActor
+@Observable
+class AppModel {
+    let immersiveSpaceID = "ImmersiveSpace"
+    enum ImmersiveSpaceState {
+        case closed
+        case inTransition
+        case open
+    }
+    var immersiveSpaceState = ImmersiveSpaceState.closed
+}
---
+++ Step Into Example Code/Step_Into_Example_CodeApp.swift
@@ -0,0 +1,34 @@
+//
+//  Step_Into_Example_CodeApp.swift
+//  Step Into Example Code
+//
+//  Created by Joseph Simpson on 10/15/24.
+//
+
+import SwiftUI
+
+@main
+struct Step_Into_Example_CodeApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+        .windowStyle(.volumetric)
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.full), in: .full)
+    }
+}
