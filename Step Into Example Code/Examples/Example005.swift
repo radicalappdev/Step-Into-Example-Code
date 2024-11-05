f35b305a816bb6c986772544c7cb8372df43be83
Joseph Simpson <joe@radicalappdev.com>
2024-11-05 05:56:04 -0500

    Revamping example 05 to go in another direction

--- Step Into Example Code/Examples/Example005.swift
+++ Step Into Example Code/Examples/Example005.swift
@@ -27,61 +27,30 @@ struct Example005: View {
                 // Lower the entire scene to the bottom of the volume
                 scene.position.y = -0.4
 
+                // Get the rocket and save it for later
                 if let toyRocket = scene.findEntity(named: "ToyRocket") {
+                    toyRocket.scale = .init(repeating: 2.5)
                     toyRocket.components.set(HoverEffectComponent())
                     rocket = toyRocket
                 }
-                
-            }
 
-            if let scene = try? await Entity(named: "GestureLabsHelpers", in: realityKitContentBundle) {
-                content.add(scene)
-
-                // Lower the entire scene to the bottom of the volume
-                scene.position.y = -0.4
-
-                if let place1 = scene.findEntity(named: "Place1") {
-                    place1.components.set(HoverEffectComponent())
-                }
-                if let place2 = scene.findEntity(named: "Place2") {
-                    place2.components.set(HoverEffectComponent())
-                }
-                if let place3 = scene.findEntity(named: "Place3") {
-                    place3.components.set(HoverEffectComponent())
+                // Remove the items we don't need
+                if let toyCar = scene.findEntity(named: "ToyCar"), let toyBiplane = scene.findEntity(named: "ToyBiplane") {
+                    toyCar.removeFromParent()
+                    toyBiplane.removeFromParent()
                 }
 
             }
+
         }
-//        .gesture(tapExample)
         .gesture(spatialTapExample)
     }
 
-    var tapExample: some Gesture {
-        TapGesture()
-            .targetedToAnyEntity()
-            .onEnded { value in
-                if let rocket = rocket {
-                    rocket.position = value.entity.position
-                }
-                print("tapped: \(value)")
-
-            }
-    }
     var spatialTapExample: some Gesture {
         SpatialTapGesture()
             .targetedToAnyEntity()
             .onEnded { value in
                 print("spatial tapped: \(value.location3D)")
-
-                if let rocket = rocket {
-
-                    let tappedPosition = value.convert(value.location3D, from: .global, to: .scene)
-                    print("spatial tapped: \(tappedPosition)")
-
-                    rocket.position = tappedPosition
-//                    rocket.setPosition(tappedPosition, relativeTo: rocket.parent)
-                }
-
 
             }
     }
