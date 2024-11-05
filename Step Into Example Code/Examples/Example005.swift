2cc43a81758dbc58b52d08d6c420dd3c4902dc4b
Joseph Simpson <joe@radicalappdev.com>
2024-11-05 06:05:33 -0500

    Adding an indicator

--- Step Into Example Code/Examples/Example005.swift
+++ Step Into Example Code/Examples/Example005.swift
@@ -17,12 +17,17 @@ import RealityKitContent
 struct Example005: View {
 
     @State var rocket: Entity?
+    @State var indicator: Entity?
 
     var body: some View {
         RealityView { content in
             // Load the scene from the Reality Kit bindle
             if let scene = try? await Entity(named: "GestureLabs", in: realityKitContentBundle) {
                 content.add(scene)
+
+                let indicatorModel = ModelEntity(
+                    mesh: .generateSphere(radius: 0.01),
+                    materials: [SimpleMaterial(color: .black, isMetallic: false)])
 
                 // Lower the entire scene to the bottom of the volume
                 scene.position.y = -0.4
@@ -31,8 +36,13 @@ struct Example005: View {
                 if let toyRocket = scene.findEntity(named: "ToyRocket") {
                     toyRocket.scale = .init(repeating: 2.5)
                     toyRocket.components.set(HoverEffectComponent())
+                    toyRocket.addChild(indicatorModel)
                     rocket = toyRocket
+                    indicator = indicatorModel
+
+
                 }
+
 
                 // Remove the items we don't need
                 if let toyCar = scene.findEntity(named: "ToyCar"), let toyBiplane = scene.findEntity(named: "ToyBiplane") {
@@ -51,6 +61,13 @@ struct Example005: View {
             .targetedToAnyEntity()
             .onEnded { value in
                 print("spatial tapped: \(value.location3D)")
+
+                
+                if let rocket = rocket, let indicator = indicator {
+                    let tappedPosition = value.convert(value.location3D, from: .global, to: .scene)
+                    print("spatial tapped position: \(tappedPosition)")
+                    indicator.position = tappedPosition
+                }
 
             }
     }
