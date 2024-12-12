abd902d8032d8214a15ce49ec9b333d85603b71f
Joseph Simpson <joe@radicalappdev.com>
2024-12-12 06:11:06 -0500

    Starting example 018

---
+++ Step Into Example Code/Examples/Example018.swift
@@ -0,0 +1,112 @@
+//  Step Into Vision - Example Code
+//
+//  Title: Example018
+//
+//  Subtitle:
+//
+//  Description:
+//
+//  Type:
+//
+//  Created by Joseph Simpson on 12/12/24.
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+import ARKit
+
+struct Example018: View {
+    let arSession = ARKitSession()
+    let handTrackingProvider = HandTrackingProvider()
+    let leftCollection = Entity()
+    let rightCollection = Entity()
+
+    let tipJoints: [HandSkeleton.JointName] = [
+        .thumbTip, .indexFingerTip, .middleFingerTip, .ringFingerTip, .littleFingerTip
+    ]
+
+    var body: some View {
+        RealityView { content in
+            content.add(leftCollection)
+            content.add(rightCollection)
+
+            if let scene = try? await Entity(named: "HandTrackingLabs", in: realityKitContentBundle) {
+                content.add(scene)
+
+                if let leftHandSphere = scene.findEntity(named: "StepSphereBlue") {
+                    // Create clones of the left hand sphere for each joint
+                    for jointName in tipJoints {
+                        let sphere = leftHandSphere.clone(recursive: true)
+                        sphere.name = jointName.description
+                        leftCollection.addChild(sphere)
+                    }
+                    leftHandSphere.isEnabled = false
+                }
+
+                if let rightHandSphere = scene.findEntity(named: "StepSphereGreen") {
+                    // Create clones of the right hand sphere for each joint
+                    for jointName in tipJoints {
+                        let sphere = rightHandSphere.clone(recursive: true)
+                        sphere.name = jointName.description
+                        rightCollection.addChild(sphere)
+                    }
+                    rightHandSphere.isEnabled = false
+                }
+            }
+        }
+        .persistentSystemOverlays(.hidden)
+
+        .task { try! await arSession.run([handTrackingProvider]) }
+
+        // Left Hand: Receive updates from the provider and process them over time
+        .task {
+            for await update in handTrackingProvider.anchorUpdates where update.anchor.chirality == .left {
+                let handAnchor = update.anchor
+                for jointName in tipJoints {
+                    if let joint = handAnchor.handSkeleton?.joint(jointName),
+                       let sphere = leftCollection.findEntity(named: jointName.description) {
+                        let transform = handAnchor.originFromAnchorTransform
+                        let jointTransform = joint.anchorFromJointTransform
+                        sphere.setTransformMatrix(transform * jointTransform, relativeTo: nil)
+                    }
+                }
+            }
+        }
+        // Right Hand: Receive updates from the provider and process them over time
+//        .task {
+//            for await update in handTrackingProvider.anchorUpdates where update.anchor.chirality == .right {
+//                let handAnchor = update.anchor
+//                for jointName in tipJoints {
+//                    if let joint = handAnchor.handSkeleton?.joint(jointName),
+//                       let sphere = rightCollection.findEntity(named: jointName.description) {
+//                        let transform = handAnchor.originFromAnchorTransform
+//                        let jointTransform = joint.anchorFromJointTransform
+//                        sphere.setTransformMatrix(transform * jointTransform, relativeTo: nil)
+//                    }
+//                }
+//            }
+//        }
+        // Alternate version using latestAnchors instead of anchorUpdates
+//        .task {
+//            while true {
+//                let anchors = handTrackingProvider.latestAnchors
+//                if let handAnchor = anchors.rightHand {
+//                    for jointName in tipJoints {
+//                        if let joint = handAnchor.handSkeleton?.joint(jointName),
+//                           let sphere = rightCollection.findEntity(named: jointName.description) {
+//                            let transform = handAnchor.originFromAnchorTransform
+//                            let jointTransform = joint.anchorFromJointTransform
+//                            sphere.setTransformMatrix(transform * jointTransform, relativeTo: nil)
+//                        }
+//                    }
+//                }
+//                try? await Task.sleep(for: .milliseconds(8)) // ~120fps
+//            }
+//        }
+
+    }
+}
+
+#Preview {
+    Example018()
+}
