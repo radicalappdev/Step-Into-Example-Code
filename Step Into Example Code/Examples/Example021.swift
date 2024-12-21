bb7a89a5cf28b896eb3c6dbeba08bd4a8ea7a8f7
Joseph Simpson <joe@radicalappdev.com>
2024-12-21 07:34:45 -0500

    Finish example 021

--- Step Into Example Code/Examples/Example021.swift
+++ Step Into Example Code/Examples/Example021.swift
@@ -23,48 +23,50 @@ struct Example021: View {
                 content.add(scene)
 
                 // 1. Set up a Spatial Tracking Session with hand tracking.
-                // This will add ARKit features to our Anchor Entities.
+                // This will add ARKit features to our Anchor Entities, enabling collisions.
                 let configuration = SpatialTrackingSession.Configuration(
                     tracking: [.hand])
                 let session = SpatialTrackingSession()
                 await session.run(configuration)
 
-                if let subject = scene.findEntity(named: "StepSphereRed"), let leftHandSphere = scene.findEntity(named: "StepSphereBlue"), let rightHandSphere = scene.findEntity(named: "StepSphereGreen") {
+                if let subject = scene.findEntity(named: "StepSphereRed"), let stepSphereBlue = scene.findEntity(named: "StepSphereBlue"), let stepSphereGreen = scene.findEntity(named: "StepSphereGreen") {
                     content.add(subject)
 
-                    // 2. Anchor some spheres to our index fingers
-                    // Set up left index finger sphere
-                    leftHandSphere.name = "leftIndex"
+                    // 2. Create an anchor for the left index finger
                     let leftIndexAnchor = AnchorEntity(.hand(.left, location: .indexFingerTip), trackingMode: .continuous)
 
                     // 3. Disable the default physics simulation on the anchor
                     leftIndexAnchor.anchoring.physicsSimulation = .none
-                    leftIndexAnchor.addChild(leftHandSphere)
+
+                    // 4. Add the sphere to the anchor and add the anchor to the scene graph
+                    leftIndexAnchor.addChild(stepSphereBlue)
                     content.add(leftIndexAnchor)
 
-
-                    // Set up right index finger sphere
-                    rightHandSphere.name = "rightIndex"
+                    // Repeat the same steps for the right index finger
                     let rightIndexAnchor = AnchorEntity(.hand(.right, location: .indexFingerTip), trackingMode: .continuous)
                     rightIndexAnchor.anchoring.physicsSimulation = .none //
-                    rightIndexAnchor.addChild(rightHandSphere.clone(recursive: true))
+                    rightIndexAnchor.addChild(stepSphereGreen)
                     content.add(rightIndexAnchor)
 
+                    // Example 1: Any entity can collide with any entity. Fire a particle burst
+                    // Allow collision between the hand anchors
+                    // Allow collision between a hand anchor and the subject
                     _ = content.subscribe(to: CollisionEvents.Began.self)  { collisionEvent in
                         print("Collision unfiltered \(collisionEvent.entityA.name) and \(collisionEvent.entityB.name)")
                         collisionEvent.entityA.components[ParticleEmitterComponent.self]?.burst()
-
                     }
 
+                    // Example 2: Only track collisions on the subject. Swap the color of the material based on left or right hand.
                     _ = content
                         .subscribe(to: CollisionEvents.Began.self, on: subject)  { collisionEvent in
                             print("Collision Subject Color Change \(collisionEvent.entityA.name) and \(collisionEvent.entityB.name)")
-                            if(collisionEvent.entityB.name == "leftIndex") {
+                            if(collisionEvent.entityB.name == "StepSphereBlue") {
                                 swapColorEntity(subject, color: .stepBlue)
-                            } else if (collisionEvent.entityB.name == "rightIndex") {
+                            } else if (collisionEvent.entityB.name == "StepSphereGreen") {
                                 swapColorEntity(subject, color: .stepGreen)
                             }
                         }
+
                 }
             }
         }
