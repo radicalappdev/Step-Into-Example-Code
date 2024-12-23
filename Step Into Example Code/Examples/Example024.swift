9123ddb9643bbe214aef079f352b3269c634dc59
Joseph Simpson <joe@radicalappdev.com>
2024-12-23 17:14:28 -0500

    Document Example 024

--- Step Into Example Code/Examples/Example024.swift
+++ Step Into Example Code/Examples/Example024.swift
@@ -14,13 +14,17 @@ import SwiftUI
 import RealityKit
 import RealityKitContent
 
+// FYI, this is only an example for how to access and display the transform data from an AnchorEntity
 struct Example024: View {
-    @State private var session: SpatialTrackingSession?
+    @State var session: SpatialTrackingSession?
     @State var leftHandTransform: Transform?
-    @State private var leftHandAnchor: AnchorEntity?
+    @State var leftHandAnchor: AnchorEntity?
 
     var body: some View {
         RealityView { content, attachments in
+
+            // 1. Set up a Spatial Tracking Session with hand tracking.
+            // This will add ARKit features to our Anchor Entities, enabling access to transforms, collisions, and physics.
             let configuration = SpatialTrackingSession.Configuration(
                 tracking: [.hand]
             )
@@ -31,31 +35,23 @@ struct Example024: View {
             if let scene = try? await Entity(named: "HandTrackingLabsTransform", in: realityKitContentBundle) {
                 content.add(scene)
 
+                // 2. Attach a sphere to our anchor so we can see something in the scene
                 if let leftHandSphere = scene.findEntity(named: "LeftHand") {
                     let leftHand = AnchorEntity(.hand(.left, location: .indexFingerTip))
                     leftHand.addChild(leftHandSphere.clone(recursive: true))
                     leftHand.anchoring.physicsSimulation = .none
                     content.add(leftHand)
                     self.leftHandAnchor = leftHand
-
-//                    Task {
-//                        while true {
-//                            if let anchor = leftHandAnchor {
-//                                leftHandTransform = Transform(matrix: anchor.transformMatrix(relativeTo: nil))
-//                            }
-//                            try? await Task.sleep(for: .seconds(1/30))
-//                        }
-//                    }
                 }
 
                 if let redSphere = scene.findEntity(named: "StepSphereRed"), let panel = attachments.entity(for: "AttachmentContent") {
-
                     redSphere.addChild(panel)
                     panel.setPosition([0, 0.2 ,0], relativeTo: redSphere)
                     panel.components[BillboardComponent.self] = .init()
                     content.add(redSphere)
                 }
             }
+
         } update: { content, attachments in
 
         } attachments: {
@@ -79,10 +75,16 @@ struct Example024: View {
             }
         }
         .modifier(DragGestureImproved())
+        .persistentSystemOverlays(.hidden)
+        .upperLimbVisibility(.hidden)
         .task {
-
             while true {
+                // 3. Periodically check the anchor transform and stash it in SwiftUi state.
+                // This will update our attachment
                 if let anchor = leftHandAnchor {
+                    // anchor.transform does not work. This seems to be the anchored entity's transform relative to the anchor itself, which we are never changing.
+                    // leftHandTransform = anchor.transform
+                    // Instead we can access the anchors global transform
                     leftHandTransform = Transform(matrix: anchor.transformMatrix(relativeTo: nil))
                 }
                 try? await Task.sleep(for: .seconds(1/30))
