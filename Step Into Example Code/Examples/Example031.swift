cbe0b50964dbe194235d84f26799bd6bb09f87a1
Joseph Simpson <joe@radicalappdev.com>
2025-01-06 16:38:30 -0500

    Going another direction with 031

--- Step Into Example Code/Examples/Example031.swift
+++ Step Into Example Code/Examples/Example031.swift
@@ -16,74 +16,32 @@ import RealityKitContent
 
 struct Example031: View {
 
-    @State var anchor: AnchoringComponent.Target.Classification = .table
-    @State var session: SpatialTrackingSession?
+    @State var trackingSession: SpatialTrackingSession = SpatialTrackingSession()
 
     var body: some View {
-        RealityView {content, attachments in
-
-            let configuration = SpatialTrackingSession.Configuration(
-                tracking: [.hand])
-            let session = SpatialTrackingSession()
-            await session.run(configuration)
-
-            
+        RealityView { content in
 
             if let scene = try? await Entity(named: "AnchorLabs", in: realityKitContentBundle) {
                 content.add(scene)
 
-                if let subject = scene.findEntity(named: "ToyRocket") {
+                if let subject = scene.findEntity(named: "CubeRed") {
 
-                    let anchorComponent = AnchoringComponent(
-                        .plane(.any, classification: anchor, minimumBounds: SIMD2(x: 0.1, y: 0.1))
-                    )
-                    subject.components.set(anchorComponent)
-
-                    _ = content.subscribe(to: SceneEvents.AnchoredStateChanged.self, { event in
-                        print("anchor changed")
-                        event.anchor.position = [0, 0.1, 0]
-
-                    })
                 }
-
             }
 
-            if let panel = attachments.entity(for: "AttachmentContent") {
-                panel.position = [-0.3, 1.2, -1]
-                content.add(panel)
-            }
-
-
-        } update: { content, attachments in
-
-            if let subject = content.entities.first?.findEntity(named: "ToyRocket") {
-
-                let anchorComponent = AnchoringComponent(.plane(.any, classification: anchor, minimumBounds: SIMD2(x: 0.1, y: 0.1)))
-                subject.setPosition([0,0,0], relativeTo: nil)
-                subject.components.set(anchorComponent)
-
-
-            }
-
-
-
-        } attachments: {
-            Attachment(id: "AttachmentContent") {
-                VStack {
-                    Button(action: {
-                        anchor = .table
-                    }, label: {
-                        Text("Table")
-                    })
-                    Button(action: {
-                        anchor = .wall
-                    }, label: {
-                        Text("Wall")
-                    })
-                }
-                .padding()
-                .glassBackgroundEffect()
-            }
+        }
+        .task {
+            await runTrackingSession()
         }
     }
+
+    func runTrackingSession() async {
+
+        let configuration = SpatialTrackingSession.Configuration(tracking: [.plane])
+
+
+        let _ = await trackingSession.run(configuration)
+    }
+
+
 }
