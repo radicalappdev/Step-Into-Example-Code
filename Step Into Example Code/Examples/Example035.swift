8476eb74afd4113550063063fb7aa33076495565
Joseph Simpson <joe@radicalappdev.com>
2025-01-10 14:30:33 -0500

    stubbing some custom hovers

--- Step Into Example Code/Examples/Example035.swift
+++ Step Into Example Code/Examples/Example035.swift
@@ -44,6 +44,26 @@ struct Example035: View {
             .frame(height: 100)
 
             HStack(spacing: 24) {
+
+                Capsule()
+                    .foregroundStyle(.regularMaterial)
+                    .overlay(Text("Custom: fade"))
+                    .hoverEffect(FadeHoverEffect())
+
+                Capsule()
+                    .foregroundStyle(.regularMaterial)
+                    .overlay(Text("Custom: scale"))
+                    .hoverEffect(.highlight, isEnabled: enableHoverEffect)
+                    .hoverEffect(ScaleHoverEffect())
+
+                Capsule()
+                    .foregroundStyle(.regularMaterial)
+                    .overlay(Text("lift"))
+                    .hoverEffect(.lift, isEnabled: enableHoverEffect)
+            }
+            .frame(height: 100)
+
+            HStack(spacing: 24) {
                 Capsule()
                     .foregroundStyle(.regularMaterial)
                     .overlay(Text("automatic"))
@@ -59,13 +79,37 @@ struct Example035: View {
                     .overlay(Text("lift"))
                     .hoverEffect(isEnabled: enableHoverEffect)
             }
+            .frame(height: 100)
             .defaultHoverEffect(.highlight)
 
-            .frame(height: 100)
         }
         .padding()
     }
 }
+
+struct FadeHoverEffect: CustomHoverEffect {
+    func body(content: Content) -> some CustomHoverEffect {
+        content.hoverEffect { effect, isActive, proxy in
+            effect.animation(.easeOut) {
+                $0.opacity(isActive ? 0.5 : 1)
+            }
+        }
+    }
+}
+
+struct ScaleHoverEffect: CustomHoverEffect {
+    func body(content: Content) -> some CustomHoverEffect {
+        content.hoverEffect { effect, isActive, proxy in
+            effect.animation(.easeOut) {
+                $0.scaleEffect(
+                    isActive ? CGSize(width: 1.2, height: 1.2) : CGSize(width: 1, height: 1),
+                    anchor: .center
+                )
+            }
+        }
+    }
+}
+
 
 #Preview {
     Example035()
