9a1f5e3b62aed644eecda3b693494a1512be1ba6
Joseph Simpson <joe@radicalappdev.com>
2025-01-26 07:00:22 -0500

    Wrapping up example 040

--- Step Into Example Code/Examples/Example040.swift
+++ Step Into Example Code/Examples/Example040.swift
@@ -1,19 +1,27 @@
+//  Step Into Vision - Example Code
 //
-//  Example040.swift
-//  Step Into Example Code
+//  Title: Example040
+//
+//  Subtitle: RealityKit Basics: RealityView
+//
+//  Description: Exploring a SwiftUI view that contains RealityView content.
+//
+//  Type: Volume
 //
 //  Created by Joseph Simpson on 1/26/25.
-//
 
 import SwiftUI
 import RealityKit
 
 struct Example040: View {
 
-    @State var istTransparent = false
+    @State var isTransparent = false
 
     var body: some View {
         RealityView { content, attachments in
+
+            // wait for 2 seconds so we can see the placeholder view
+            try? await Task.sleep(for: .seconds(2))
 
             let subjectEntity = Entity()
             subjectEntity.name = "Subject"
@@ -39,33 +47,52 @@ struct Example040: View {
             sphere.setPosition([-0.25, 0.2, 0.1], relativeTo: subjectEntity)
             subjectEntity.addChild(sphere)
 
-            
+            // Get the subject panel from the attachments
+            // Once loaded we can add attachments to the content or add them as children to other entities
+            if let subjectPanel = attachments.entity(for: "subjectPanel") {
+                subjectPanel.setPosition([0, 0.3, 0], relativeTo: subjectEntity)
+                content.add(subjectPanel)
+            }
 
         } update: { content, attachments in
-
+            // This update closure will fire anytime isTransparent is modififed.
+            // We'll query the scene graph in content for the entity we want to update
             if let subject = content.entities.first(where: { $0.name == "Subject" }) {
-
-                subject.components.set(OpacityComponent(opacity: istTransparent ? 0.5 : 1.0 ))
-
-
+                // We can set a new instance of the OpacityComponent on the entity. This will replace any existing OpacityComponent. We don't need to remove the old one first.
+                subject.components.set(OpacityComponent(opacity: isTransparent ? 0.5 : 1.0 ))
             }
 
         } placeholder: {
-
-            ProgressView()
+            // A SwiftUI view that will display while the RealityView is loading
+            VStack {
+                Text("Loading")
+                    .padding(EdgeInsets(top: 6, leading: 12, bottom: 6, trailing: 12))
+                ProgressView()
+            }
+            .font(.largeTitle)
+            .padding()
+            .glassBackgroundEffect()
 
         } attachments: {
-
-        }
-        .toolbar {
-            ToolbarItem(placement: .bottomOrnament, content: {
-                Toggle(isOn: $istTransparent, label: {
-                    Text("Opacity")
-                })
-                .toggleStyle(.button)
+            // A collection of attachments that contain SwiftUI Views. RealityKit will convert each of these into entities that we can place in our scene using the make and update closures.
+            Attachment(id: "subjectPanel", {
+                VStack {
+                    HStack {
+                        Image(systemName: "circle.fill")
+                        Text("Subject")
+                    }
+                    .font(.largeTitle)
+                    Toggle(isOn: $isTransparent, label: {
+                        Text("Opacity")
+                    })
+                    .toggleStyle(.button)
+                }
+                .padding()
+                .glassBackgroundEffect()
             })
 
         }
+
     }
 }
 
