763472ba9ef5317ced74a64919b69626b0e0bc0e
Joseph Simpson <joe@radicalappdev.com>
2025-03-26 12:09:17 -0400

    Wrapping up example 059

--- Step Into Example Code/Examples/Example059.swift
+++ Step Into Example Code/Examples/Example059.swift
@@ -17,9 +17,9 @@ import RealityKitContent
 struct Example059: View {
 
     @State var collisionBeganUnfiltered: EventSubscription?
-
-    @State var collisionBeganSubject: EventSubscription?
-    @State var collisionEndedSubject: EventSubscription?
+    @State var collisionSubjectBegan: EventSubscription?
+    @State var collisionSubjectUpdated: EventSubscription?
+    @State var collisionSubjectEnded: EventSubscription?
 
     var body: some View {
         RealityView { content in
@@ -43,28 +43,47 @@ struct Example059: View {
                 collisionEvent.entityA.components[ParticleEmitterComponent.self]?.burst()
             }
 
-
             // Example 2: Only trigger collisions on the subject.
             // Subject will always be entityA in this event
-            collisionBeganSubject = content
+            collisionSubjectBegan = content
                 .subscribe(to: CollisionEvents.Began.self, on: subject)  { collisionEvent in
-                    print("Collision Subject Bounce \(collisionEvent.entityA.name) and \(collisionEvent.entityB.name)")
+                    print("Collision Subject Example \(collisionEvent.entityA.name) and \(collisionEvent.entityB.name)")
 
                     // When we reach the Base, we'll fire the particle burst on the other three spheres
                     if(collisionEvent.entityB.name == "Base") {
                         stepSphereRed.components[ParticleEmitterComponent.self]?.burst()
                         stepSphereGreen.components[ParticleEmitterComponent.self]?.burst()
                         stepSphereBlue.components[ParticleEmitterComponent.self]?.burst()
-                    // If we reach a color sphere, change the subject color
-                    } else if(collisionEvent.entityB.name == "StepSphereRed") {
+                    }
+                }
+
+            // Example 3: We can use Updated to fire an event for each frame that the subject is in contact with the Base
+            collisionSubjectUpdated = content
+                .subscribe(to: CollisionEvents.Updated.self, on: subject)  { collisionEvent in
+                    print("Collision Subject Grow \(collisionEvent.entityA.name) and \(collisionEvent.entityB.name)")
+                    if(collisionEvent.entityB.name == "StepSphereRed") {
+                        collisionEvent.entityB.scale *= 1.002
+                    } else if(collisionEvent.entityB.name == "StepSphereGreen") {
+                        collisionEvent.entityB.scale *= 0.998
+                    }
+                }
+
+            // Example 4: We can reset the scale on ended. We'll also change the subject color
+            collisionSubjectEnded = content
+                .subscribe(to: CollisionEvents.Ended.self, on: subject)  { collisionEvent in
+                    print("Collision Subject Reset \(collisionEvent.entityA.name) and \(collisionEvent.entityB.name)")
+
+                    if(collisionEvent.entityB.name == "StepSphereRed") {
                         swapColorEntity(subject, color: .stepRed)
                     } else if (collisionEvent.entityB.name == "StepSphereGreen") {
                         swapColorEntity(subject, color: .stepGreen)
                     } else if (collisionEvent.entityB.name == "StepSphereBlue") {
                         swapColorEntity(subject, color: .stepBlue)
+                    } else {
+                        return
                     }
+                    collisionEvent.entityB.scale = .init(repeating: 0.8)
                 }
-
 
         }
     }
@@ -82,3 +101,8 @@ struct Example059: View {
 #Preview {
     Example059()
 }
+
+
+
+
+
