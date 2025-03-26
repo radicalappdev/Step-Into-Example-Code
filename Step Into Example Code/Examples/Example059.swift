89f9d53b8ee3eae63a98180de1a7780c3d63d742
Joseph Simpson <joe@radicalappdev.com>
2025-03-26 11:17:59 -0400

    Combine examples

--- Step Into Example Code/Examples/Example059.swift
+++ Step Into Example Code/Examples/Example059.swift
@@ -16,10 +16,9 @@ import RealityKitContent
 
 struct Example059: View {
 
-    @State var subject: Entity?
     @State var collisionBeganUnfiltered: EventSubscription?
+
     @State var collisionBeganSubject: EventSubscription?
-    @State var collisionBeganSubjectColor: EventSubscription?
     @State var collisionEndedSubject: EventSubscription?
 
     var body: some View {
@@ -30,18 +29,54 @@ struct Example059: View {
             content.add(scene)
             scene.position.y = -0.4
 
-            subject = scene.findEntity(named: "Subject")
+            guard let subject = scene.findEntity(named: "Subject"),
+                  let stepSphereRed = scene.findEntity(named: "StepSphereRed"),
+                  let stepSphereGreen = scene.findEntity(named: "StepSphereGreen"),
+                  let stepSphereBlue = scene.findEntity(named: "StepSphereBlue")
+            else { return }
+
 
             // Example 1: This unfiltered event will fire twice. Once for each entity in the collision
             // Each will be entityA in their own version of the event, so we don't need the burst for entityB
             collisionBeganUnfiltered = content.subscribe(to: CollisionEvents.Began.self)  { collisionEvent in
                 print("Collision unfiltered \(collisionEvent.entityA.name) and \(collisionEvent.entityB.name)")
                 collisionEvent.entityA.components[ParticleEmitterComponent.self]?.burst()
-
             }
+
+
+            // Example 2: Only trigger collisions on the subject.
+            // Subject will always be entityA in this event
+            collisionBeganSubject = content
+                .subscribe(to: CollisionEvents.Began.self, on: subject)  { collisionEvent in
+                    print("Collision Subject Bounce \(collisionEvent.entityA.name) and \(collisionEvent.entityB.name)")
+
+                    // When we reach the Base, we'll fire the particle burst on the other three spheres
+                    if(collisionEvent.entityB.name == "Base") {
+                        stepSphereRed.components[ParticleEmitterComponent.self]?.burst()
+                        stepSphereGreen.components[ParticleEmitterComponent.self]?.burst()
+                        stepSphereBlue.components[ParticleEmitterComponent.self]?.burst()
+                    // If we reach a color sphere, change the subject color
+                    } else if(collisionEvent.entityB.name == "StepSphereRed") {
+                        swapColorEntity(subject, color: .stepRed)
+                    } else if (collisionEvent.entityB.name == "StepSphereGreen") {
+                        swapColorEntity(subject, color: .stepGreen)
+                    } else if (collisionEvent.entityB.name == "StepSphereBlue") {
+                        swapColorEntity(subject, color: .stepBlue)
+                    }
+                }
+
 
         }
     }
+
+    func swapColorEntity(_ entity: Entity, color: UIColor) {
+        if var mat = entity.components[ModelComponent.self]?.materials.first as? PhysicallyBasedMaterial {
+            mat.baseColor = .init(tint: color)
+            entity.components[ModelComponent.self]?.materials[0] = mat
+        }
+    }
+
+
 }
 
 #Preview {
