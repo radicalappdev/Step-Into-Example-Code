c9485224cf314f8f732ae5efbc497f5b4709b4ed
Joseph Simpson <joe@radicalappdev.com>
2025-04-01 14:08:40 -0400

    Adding traffic cone example

--- Step Into Example Code/Examples/Example061.swift
+++ Step Into Example Code/Examples/Example061.swift
@@ -16,27 +16,47 @@ import RealityKitContent
 
 struct Example061: View {
     var body: some View {
-        RealityView { content, attachments in
+        RealityView {
+ content,
+ attachments in
             guard let scene = try? await Entity(named: "Caution", in: realityKitContentBundle) else { return }
             content.add(scene)
 
-            if let wetFloorSign = scene.findEntity(named: "wet_floor_sign"), let front = attachments.entity(for: "wet_floor_front") {
+            if let wetFloorSign = scene.findEntity(named: "wet_floor_sign"),
+                let wetFloorAttachment = attachments.entity(for: "wet_floor_attachment") {
                 // The wet_floor_sign asset was converted from another format. It was scaled to 0.01 on all axes to fit in this scene.
                 // We'll have to scale the attachment to compensate for the scale of the entity
 
-                // Ad the front attachment as a child of the wet floor sign
-                wetFloorSign.addChild(front)
+                // Ad the wetFloorAttachment attachment as a child of the wet floor sign
+                wetFloorSign.addChild(wetFloorAttachment)
 
                 // Adjust the transform to position it just in front of the sign
-                let frontTransform = Transform(scale: .init(repeating: 200), rotation: simd_quatf(Rotation3D(angle: Angle2D(degrees: 11), axis: RotationAxis3D(x: -1, y: 0, z: 0))), translation: [0, 30, 6.7])
-                front.transform = frontTransform
+                let transform = Transform(scale: .init(repeating: 200), rotation: simd_quatf(Rotation3D(angle: Angle2D(degrees: 11), axis: RotationAxis3D(x: -1, y: 0, z: 0))), translation: [0, 30, 6.7])
+                wetFloorAttachment.transform = transform
+
+            }
+
+            if let trafficCone = scene.findEntity(named: "traffic_cone_02"),
+               let traffiConeAttachment = attachments.entity(for: "traffic_cone_attachment") {
+
+                // Adjust the transform to position it just in front of the sign
+                let transform = Transform(
+                    scale: .init(repeating: 1.0),
+                    rotation: simd_quatf(
+                        Rotation3D(angle: Angle2D(degrees: -24), axis: RotationAxis3D(x: 0, y: 1, z: 0))
+                    ),
+                    translation: trafficCone.position + [0, 0.8 , 0]
+                )
+
+                traffiConeAttachment.transform = transform
+                content.add(traffiConeAttachment)
 
             }
 
         } update: { content, attachments in
 
         } attachments: {
-            Attachment(id: "wet_floor_front") {
+            Attachment(id: "wet_floor_attachment") {
                 VStack(spacing: 24) {
                     Text("CAUTION")
                         .font(.largeTitle)
@@ -52,6 +72,26 @@ struct Example061: View {
                 }
                 .foregroundStyle(.black)
                 .padding()
+            }
+
+            Attachment(id: "traffic_cone_attachment") {
+                VStack(spacing: 24) {
+                    Text("Watch Out")
+                        .font(.extraLargeTitle)
+                    ZStack {
+                        Image(systemName: "triangle")
+                            .font(.system(size: 96, weight: .semibold))
+                        Image(systemName: "eyes")
+                            .font(.system(size: 36, weight: .heavy))
+                            .offset(y:12)
+                    }
+                    Text("for traffic cones")
+                        .font(.extraLargeTitle)
+                }
+                .padding(24)
+                .foregroundStyle(.white)
+                .background(.trafficOrange)
+                .clipShape(.rect(cornerRadius: 24.0))
             }
         }
         .modifier(DragGestureImproved())
