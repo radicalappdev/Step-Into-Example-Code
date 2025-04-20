3dbb2967639764cf862caff12cf857c5382e220c
Joseph Simpson <joe@radicalappdev.com>
2025-04-20 12:41:48 -0400

    Stubbing in a frame

--- Step Into Example Code/Examples/Example074.swift
+++ Step Into Example Code/Examples/Example074.swift
@@ -18,63 +18,32 @@ import ARKit
 struct Example074: View {
     @State var session = ARKitSession()
 
-    @State var planeAnchorsSimple: [UUID: Entity] = [:]
-
-    // An entity with physics that can bounce around the room
-    @State var subject : ModelEntity = {
-        let subject = ModelEntity(
-            mesh: .generateSphere(radius: 0.1),
-            materials: [SimpleMaterial(color: .stepRed, isMetallic: false)])
-        subject.setPosition([1, 1, -1], relativeTo: nil)
-
-        let collision = CollisionComponent(shapes: [.generateSphere(radius: 0.1)])
-
-        var physics = PhysicsBodyComponent(
-            shapes: [.generateSphere(radius: 0.1)],
-            mass: 1.0,
-            material: .generate(friction: 0, restitution: 1),
-            mode: .dynamic
-        )
-        physics.isAffectedByGravity = false
-
-        let input = InputTargetComponent()
-        subject.components.set([collision, physics, input])
-
-        return subject
-    }()
-
+    @State var frame = Entity()
+    @State var portal = Entity()
+    @State var wallAnchor: PlaneAnchor?
 
     var body: some View {
         RealityView { content in
-            content.add(subject)
+            guard let scene = try? await Entity(named: "PortalFrame", in: realityKitContentBundle) else { return }
+            content.add(scene)
+
+            guard let frameEntity = scene.findEntity(named: "picture_frame_02") else { return }
+            frame = frameEntity
+            frame.isEnabled = false
+
+
         } update: { content in
 
-            for (_, entity) in planeAnchorsSimple {
-                if !content.entities.contains(entity) {
-                    content.add(entity)
-                }
-            }
+
         }
-        .gesture(TapGesture()
-            .targetedToEntity(subject)
-            .onEnded { value in
-                // Add some force when we tap the subject
-                let force = SIMD3<Float>(
-                    x: Float.random(in: -1...1),
-                    y: Float.random(in: -1...1),
-                    z: Float.random(in: -1...1)
-                )
-                var motion = PhysicsMotionComponent()
-                motion.linearVelocity = force * 3
-                value.entity.components.set(motion)
-            })
+
         .task {
             try! await setupAndRunPlaneDetection()
         }
     }
 
     func setupAndRunPlaneDetection() async throws {
-        let planeData = PlaneDetectionProvider(alignments: [.horizontal, .vertical, ])
+        let planeData = PlaneDetectionProvider(alignments: [.vertical])
         if PlaneDetectionProvider.isSupported {
             do {
                 try await session.run([planeData])
@@ -82,13 +51,25 @@ struct Example074: View {
                     switch update.event {
                     case .added, .updated:
                         let anchor = update.anchor
+                        print("anchor added or updated")
+                        if(anchor.classification == .wall) {
+                            print("WALL anchor added or updated")
+                            if wallAnchor == nil {
+                                wallAnchor = anchor
+                                frame.setTransformMatrix(wallAnchor!.originFromAnchorTransform, relativeTo: nil)
+                                frame.isEnabled = true
+                                print("WALL added a frame")
+                            }
+                        }
 
-                        let planeEntitySimple = createSimplePlaneEntity(for: anchor)
-                        planeAnchorsSimple[anchor.id] = planeEntitySimple
 
                     case .removed:
                         let anchor = update.anchor
-                        planeAnchorsSimple.removeValue(forKey: anchor.id)
+                        if (anchor == wallAnchor) {
+                            wallAnchor = nil
+                            frame.isEnabled = false
+                            print("WALL removed a frame")
+                        }
 
                     }
                 }
@@ -98,24 +79,6 @@ struct Example074: View {
         }
     }
 
-    func createSimplePlaneEntity(for anchor: PlaneAnchor) -> Entity {
-
-        let extent = anchor.geometry.extent
-        let mesh = MeshResource.generatePlane(width: extent.width, height: extent.height)
-        let material = OcclusionMaterial()
-
-        let entity = ModelEntity(mesh: mesh, materials: [material])
-        entity.transform = Transform(matrix: matrix_multiply(anchor.originFromAnchorTransform, extent.anchorFromExtentTransform))
-
-        // We'll let RealityKit generate a simple collision shape based on the entity.
-        // For more detailed shapes see: https://stepinto.vision/example-code/arkit-planedetectionprovider-adding-collisions-and-physics/
-        entity.generateCollisionShapes(recursive: true, static: true)
-        let physicsMaterial = PhysicsMaterialResource.generate(friction: 0, restitution: 1)
-        let physics = PhysicsBodyComponent(massProperties: .default, material: physicsMaterial, mode: .static)
-        entity.components.set(physics)
-
-        return entity
-    }
 }
 #Preview {
     Example074()
