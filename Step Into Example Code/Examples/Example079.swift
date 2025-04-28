1a41e99dedd0ed205cd2929b74d1a73338002de8
Joseph Simpson <joe@radicalappdev.com>
2025-04-28 15:13:32 -0400

    Going in another direction

--- Step Into Example Code/Examples/Example079.swift
+++ Step Into Example Code/Examples/Example079.swift
@@ -18,8 +18,11 @@ import ARKit
 struct Example079: View {
     @State var session = ARKitSession()
     @State var worldTracking = WorldTrackingProvider()
+
+    @State var worldAnchorEntities: [UUID: Entity] = [:]
+
+    @State var placement = Entity()
     @State var subject = Entity()
-    @State var anchorID: UUID?
 
     var body: some View {
         RealityView { content in
@@ -27,22 +30,46 @@ struct Example079: View {
             guard let scene = try? await Entity(named: "WorldTracking", in: realityKitContentBundle) else { return }
             content.add(scene)
 
-            if let subjectEntity = scene.findEntity(named: "ToyRocket") {
+            if let placementEntity = scene.findEntity(named: "PlacementPreview") {
+                subject = placementEntity
+            }
+
+            if let subjectEntity = scene.findEntity(named: "SubjectTemplate") {
                 subject = subjectEntity
             }
 
-        }
-        .modifier(DragGestureWithWorldAnchor() {
-            Task {
-                let anchor = WorldAnchor(originFromAnchorTransform: subject.transformMatrix(relativeTo: nil))
-                try await worldTracking.addAnchor(anchor)
-                
+        } update: { content in
+            for (_, entity) in worldAnchorEntities {
+                if !content.entities.contains(entity) {
+                    content.add(entity)
+                }
             }
-        })
+        }
+        .modifier(DragGestureImproved())
         .task {
             try! await setupAndRunWorldTracking()
         }
     }
+
+    var tapPlacement: some Gesture {
+        TapGesture()
+            .targetedToAnyEntity()
+            .onEnded { value in
+
+                if value.entity.name == "PlacementPreview" {
+                    // If we tapped the placement preview cube, create an anchor
+                    Task {
+                        let anchor = WorldAnchor(originFromAnchorTransform: subject.transformMatrix(relativeTo: nil))
+                        try await worldTracking.addAnchor(anchor)
+                    }
+                } else {
+                    // Get the UUID we stored on the entity
+                    let uuid = UUID(uuidString: value.entity.name) ?? UUID()
+                    // if the UUID is in the worldAnchorEntities, remove it from the data provider
+                }
+            }
+    }
+
 
 
 
@@ -55,12 +82,17 @@ struct Example079: View {
                 for await update in worldTracking.anchorUpdates {
                     switch update.event {
                     case .added, .updated:
-                        // Update the app's understanding of this world anchor.
+
+                        let subjectClone = subject.clone(recursive: true)
+                        subjectClone.name = update.anchor.id.uuidString
+
+                        worldAnchorEntities[update.anchor.id] = subjectClone
                         print("Anchor position updated. \(update.anchor.id)")
-                        subject.transform = Transform(matrix: update.anchor.originFromAnchorTransform)
+
                     case .removed:
-                        // Remove content related to this anchor.
-                        print("Anchor position now unknown.")
+
+                        print("Anchor removed. \(update.anchor.id)")
+                        worldAnchorEntities.removeValue(forKey: update.anchor.id)
                     }
                 }
             } catch {
@@ -71,60 +103,7 @@ struct Example079: View {
     }
 }
 
-fileprivate struct DragGestureWithWorldAnchor: ViewModifier {
 
-    @State var isDragging: Bool = false
-    @State var initialPosition: SIMD3<Float> = .zero
-
-    let updateAnchor: (() -> Void)?
-
-    init(updateAnchor: (() -> Void)? = nil) {
-        self.updateAnchor = updateAnchor
-    }
-
-    func body(content: Content) -> some View {
-        content
-            .gesture(
-                DragGesture()
-                    .targetedToAnyEntity()
-                    .onChanged { value in
-                        // We we start the gesture, cache the entity position
-                        if !isDragging {
-                            isDragging = true
-                            initialPosition = value.entity.position
-                        }
-
-                        guard let entityParent = value.entity.parent else { return }
-
-                        // The current location: where we are in the gesture
-                        let gesturePosition = value.convert(value.location3D, from: .global, to: entityParent)
-
-                        // Minus the start location of the gesture
-                        let deltaPosition = gesturePosition - value.convert(value.startLocation3D, from: .global, to: entityParent)
-
-                        // Plus the initial position of the entity
-                        let newPos = initialPosition + deltaPosition
-
-                        // Optional: using move(to:) to smooth out the movement
-                        let newTransform = Transform(
-                            scale: value.entity.scale,
-                            rotation: value.entity.orientation,
-                            translation: newPos
-                        )
-
-                        value.entity.move(to: newTransform, relativeTo: entityParent, duration: 0.1)
-                    }
-                    .onEnded { value in
-                        // Clean up when the gesture has ended
-                        isDragging = false
-                        initialPosition = .zero
-                        if let updateAnchor = updateAnchor {
-                            updateAnchor()
-                        }
-                    }
-            )
-    }
-}
 
 #Preview {
     Example079()
