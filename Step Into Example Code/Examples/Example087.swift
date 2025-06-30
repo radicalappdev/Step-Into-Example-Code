63838bd2cfdbd7149a34c04f3b903c7bbc3c6326
Joseph Simpson <joe@radicalappdev.com>
2025-06-30 12:24:52 -0400

    More hebaviors

--- Step Into Example Code/Examples/Example087.swift
+++ Step Into Example Code/Examples/Example087.swift
@@ -18,15 +18,22 @@ struct Example087: View {
 
     @State private var releaseBehavior: ManipulationComponent.ReleaseBehavior = .reset
     @State private var translationBehavior: ManipulationComponent.Dynamics.TranslationBehavior = .unconstrained
+    @State private var rotationBehaviorPrimary: ManipulationComponent.Dynamics.RotationBehavior = .unconstrained
+    @State private var rotationBehaviorSecondary: ManipulationComponent.Dynamics.RotationBehavior = .unconstrained
+    @State private var scalingBehavior: ManipulationComponent.Dynamics.ScalingBehavior = .unconstrained
+    @State private var inertia: ManipulationComponent.Dynamics.Inertia = .zero
 
     // An entity we can manipulate
     @State private var subject = createStepDemoBox()
 
+
+
     var body: some View {
         RealityView { content in
 
+            subject.position.y = -0.2
 
-            // We'll use configureEntity to set up input and collision
+            // We'll use configureEntity to set up input and collision on the subject
             ManipulationComponent.configureEntity(subject, collisionShapes: [.generateBox(width: 0.25, height: 0.25, depth: 0.25)])
 
             // Create the component and add it to the entity
@@ -63,6 +70,46 @@ struct Example087: View {
                     Spacer()
                     Text("\(translationBehavior == .unconstrained ? "Unconstrained" : "None")")
                 })
+
+                Button(action: {
+                    rotationBehaviorPrimary = rotationBehaviorPrimary == .unconstrained ? .none : .unconstrained
+                    if var mc = subject.components[ManipulationComponent.self] {
+                        mc.dynamics.primaryRotationBehavior = rotationBehaviorPrimary
+                        subject.components.set(mc)
+                    }
+                }, label: {
+                    Text("Primary Rotation:")
+                    Spacer()
+                    Text("\(rotationBehaviorPrimary == .unconstrained ? "Unconstrained" : "None")")
+                })
+
+                Button(action: {
+                    rotationBehaviorSecondary = rotationBehaviorSecondary == .unconstrained ? .none : .unconstrained
+                    if var mc = subject.components[ManipulationComponent.self] {
+                        mc.dynamics.primaryRotationBehavior = rotationBehaviorSecondary
+                        subject.components.set(mc)
+                    }
+                }, label: {
+                    Text("Secondary Rotation:")
+                    Spacer()
+                    Text("\(rotationBehaviorSecondary == .unconstrained ? "Unconstrained" : "None")")
+                })
+
+                Button(action: {
+                    scalingBehavior = scalingBehavior == .unconstrained ? .none : .unconstrained
+                    if var mc = subject.components[ManipulationComponent.self] {
+                        mc.dynamics.scalingBehavior = scalingBehavior
+                        subject.components.set(mc)
+                    }
+                }, label: {
+                    Text("Scaling Behavior:")
+                    Spacer()
+                    Text("\(scalingBehavior == .unconstrained ? "Unconstrained" : "None")")
+                })
+
+
+
+
             }
             .frame(width: 400)
             .padding()
