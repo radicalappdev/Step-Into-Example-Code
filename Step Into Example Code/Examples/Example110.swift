39bc8a99826fbc19723fff26aba3e1a5945db701
Joseph Simpson <joe@radicalappdev.com>
2025-10-13 11:41:33 -0400

    Adding animation examples

--- Step Into Example Code/Examples/Example110.swift
+++ Step Into Example Code/Examples/Example110.swift
@@ -18,11 +18,11 @@ struct Example110: View {
 
     @State private var subjectA = Entity()
     @State private var subjectAToggle = false
+    @State private var selectedAnimation: Animation = .easeInOut(duration: 1)
     var subjectAToggleAnimation: Binding<Bool> {
-        $subjectAToggle.animation(.easeInOut(duration: 1))
+        $subjectAToggle.animation(selectedAnimation)
     }
 
-    
     var body: some View {
         RealityView { content in
 
@@ -36,27 +36,75 @@ struct Example110: View {
         } update: { content in
 
             content.animate {
+                // Animate part of the transfrm
                 let scaler: Float = subjectAToggle ? 2.0 : 1.0
                 subjectA.scale = .init(repeating: scaler)
 
+                // Animate a material value
                 if var material = subjectA.components[ModelComponent.self]?.materials.first as? PhysicallyBasedMaterial {
                     material.baseColor.tint = UIColor(subjectAToggle ? .stepRed : .stepGreen)
                     subjectA.components[ModelComponent.self]?.materials[0] = material
                 }
 
+                // Animate a light value
                 if let spotLightEntity = subjectA.findEntity(named: "SpotLight") {
-                    
+                    spotLightEntity.components[SpotLightComponent.self]?.innerAngleInDegrees = subjectAToggle ? 45.0 : 15.0
+                    spotLightEntity.components[SpotLightComponent.self]?.innerAngleInDegrees = subjectAToggle ? 60.0 : 20.0
                 }
             }
 
         }
         .toolbar {
             ToolbarItem(placement: .bottomOrnament, content: {
-                Toggle(isOn: subjectAToggleAnimation, label: {
-                    Text("Toggle Subject A")
-                })
+                HStack {
+                    Toggle(isOn: subjectAToggleAnimation, label: {
+                        Text("Toggle")
+                    })
+                }
             })
         }
+        .ornament(attachmentAnchor: .scene(.trailing), ornament: {
+            VStack {
+                Button(action: {
+                    selectedAnimation = .easeInOut(duration: 1)
+                }, label: {
+                    Text("Ease In Out")
+                })
+
+                Button(action: {
+                    selectedAnimation = .smooth
+                }, label: {
+                    Text("Smooth")
+                })
+
+                Button(action: {
+                    selectedAnimation = .snappy
+                }, label: {
+                    Text("Snappy")
+                })
+
+                Button(action: {
+                    selectedAnimation = .bouncy
+                }, label: {
+                    Text("Bouncy")
+                })
+
+                Button(action: {
+                    selectedAnimation = .bouncy(duration: 1.0, extraBounce: 0.1)
+                }, label: {
+                    Text("Bouncy + Extra")
+                })
+
+                Button(action: {
+                    selectedAnimation = .spring(.bouncy(duration: 2.0, extraBounce: 0.1), blendDuration: 0.2)
+                }, label: {
+                    Text("Spring Bounce")
+                })
+
+            }
+            .padding()
+            .glassBackgroundEffect()
+        })
     }
 }
 
