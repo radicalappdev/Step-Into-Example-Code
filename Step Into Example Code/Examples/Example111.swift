ca4906c4c330f6a01f35b4d4c389f8a4c1311cfe
Joseph Simpson <joe@radicalappdev.com>
2025-10-14 14:15:52 -0400

    Wrapping up example 111

--- Step Into Example Code/Examples/Example111.swift
+++ Step Into Example Code/Examples/Example111.swift
@@ -2,11 +2,11 @@
 //
 //  Title: Example111
 //
-//  Subtitle:
+//  Subtitle: RealityKit Basics: SwiftUi Animation Completion
 //
-//  Description:
+//  Description: Calling code when our animation finishes.
 //
-//  Type:
+//  Type: Volume
 //
 //  Created by Joseph Simpson on 10/14/25.
 
@@ -18,7 +18,7 @@ struct Example111: View {
 
     // For this example, we'll use a state var. In a production app it makes more sense to keep entity state in components
     @State private var subjectAToggle = false
-    @State private var subjectBToggle = false
+    @State private var animationCounter: Int = 0
 
     var body: some View {
         RealityView { content in
@@ -27,32 +27,36 @@ struct Example111: View {
             content.add(subjectA)
             let tapA = TapGesture().onEnded({ [weak subjectA] _ in
                 // We'll use a SwiftUI animation to scale the entity
-                Entity.animate(.easeInOut(duration: 1), body: {
-                    subjectBToggle.toggle()
-                    let scaler: Float = subjectBToggle ? 1.5 : 1.0
-                    subjectA?.scale = .init(repeating: scaler)
+                Entity.animate(.bouncy(duration: 0.6, extraBounce: 0.2), body: {
+                    subjectAToggle.toggle()
+
+                    if let entity = subjectA {
+                        let scaler: Float = subjectAToggle ? 1.5 : 1.0
+                        entity.scale = .init(repeating: scaler)
+                    }
+
+                }, completion: {
+                    // When the animation finishes we'll increment the counter
+                    withAnimation(.easeOut) {
+                        animationCounter += 1
+                    }
                 })
             })
             let gestureA = GestureComponent(tapA)
             subjectA.components.set(gestureA)
-            subjectA.position.x = -0.2
 
-            let subjectB = createStepDemoBox("subjectB", true)
-            content.add(subjectB)
-
-            let tapB = TapGesture().onEnded({ [weak subjectB] _ in
-                // We'll use a SwiftUI animation to scale the entity
-                Entity.animate(.easeInOut(duration: 1), body: {
-                    subjectBToggle.toggle()
-                    let scaler: Float = subjectBToggle ? 1.5 : 1.0
-                    subjectB?.scale = .init(repeating: scaler)
-                })
+        }
+        .toolbar {
+            ToolbarItem(placement: .bottomOrnament, content: {
+                HStack {
+                    Text("Count:")
+                    Spacer()
+                    Text("\(animationCounter)")
+                        .contentTransition(.numericText(countsDown: false))
+                }
+                .font(.title)
+                .frame(width: 120)
             })
-            let gestureB = GestureComponent(tapB)
-            subjectB.components.set(gestureB)
-            subjectB.position.x = 0.2
-            subjectB.transform.rotation = .init(angle: .pi / 4, axis: [0, 1, 0])
-
         }
     }
 }
