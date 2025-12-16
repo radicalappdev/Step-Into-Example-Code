667213b100d7086b30f20195ff781a262bf2fa1d
Joseph Simpson <joe@radicalappdev.com>
2025-12-16 15:50:57 -0500

    Adding some materials

--- Step Into Example Code/Examples/Example136.swift
+++ Step Into Example Code/Examples/Example136.swift
@@ -19,55 +19,40 @@ import RealityKitContent
 struct Example136: View {
     var body: some View {
         RealityView { content in
-
-            let entity = makeTextEntity(titleA: "Step Into", titleB: "Vision")
+            
+            let entity = makeTextEntity(title: "visionOS")
             content.add(entity)
-
+            
         }
         .realityViewLayoutBehavior(.centered)
     }
+    
+    let mat1 = SimpleMaterial(color: .stepBackgroundPrimary, roughness: 0.2, isMetallic: false)
+    let mat2 = SimpleMaterial(color: .stepGreen, roughness: 0.2, isMetallic: false)
+    let mat3 = SimpleMaterial(color: .black, roughness: 0.2, isMetallic: false)
 
-    @MainActor private let faceMaterial: PhysicallyBasedMaterial = {
-        var faceMaterial = PhysicallyBasedMaterial()
-        faceMaterial.metallic = .init(floatLiteral: 1)
-        faceMaterial.roughness = .init(floatLiteral: 0.85)
-        faceMaterial.baseColor = .init(tint: #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1))
-        return faceMaterial
-    }()
-
-    @MainActor private let borderMaterial: PhysicallyBasedMaterial = {
-        var borderMaterial = PhysicallyBasedMaterial()
-        borderMaterial.metallic = .init(floatLiteral: 0.15)
-        borderMaterial.roughness = .init(floatLiteral: 0.85)
-        borderMaterial.baseColor = .init(tint: #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1))
-        return borderMaterial
-    }()
-
-    private func makeTextEntity(titleA: String, titleB: String) -> ModelEntity {
-
-        var textString = AttributedString(titleA)
-        textString.font = .monospacedSystemFont(ofSize: 8.0, weight: .light)
-
-        let attributes = AttributeContainer([.font: Font.system(size: 8.0, weight: .heavy)])
-        textString.append(AttributedString("\(titleB)", attributes: attributes))
+    private func makeTextEntity(title: String) -> ModelEntity {
+        
+        var textString = AttributedString(title)
+        textString.font = .system(size: 8.0, weight: .medium, design: .rounded)
 
         let paragraphStyle = NSMutableParagraphStyle()
         paragraphStyle.alignment = .center
         textString.mergeAttributes(AttributeContainer([.paragraphStyle: paragraphStyle]))
-
+        
         var textOptions = MeshResource.GenerateTextOptions()
-        textOptions.containerFrame = CGRect(x: 0, y: 0, width: 80, height: 50)
+        textOptions.containerFrame = CGRect(x: 0, y: 0, width: 100, height: 50)
 
         var extrusionOptions = MeshResource.ShapeExtrusionOptions()
-        extrusionOptions.extrusionMethod = .linear(depth: 1)
-        extrusionOptions.materialAssignment = .init(front: 0, back: 0, extrusion: 1, frontChamfer: 1, backChamfer: 1)
+        extrusionOptions.extrusionMethod = .linear(depth: 2)
+        extrusionOptions.materialAssignment = .init(front: 0, back: 0, extrusion: 1, frontChamfer: 2, backChamfer: 2)
         extrusionOptions.chamferRadius = 0.1
-
+        
         let textMesh = try! MeshResource(extruding: textString,
                                          textOptions: textOptions,
                                          extrusionOptions: extrusionOptions)
-
-        return ModelEntity(mesh: textMesh, materials: [faceMaterial, borderMaterial])
+        
+        return ModelEntity(mesh: textMesh, materials: [mat1, mat2, mat3])
     }
 }
 
