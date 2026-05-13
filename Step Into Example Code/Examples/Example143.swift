aebb0c5ee7b2877ba49263aa6598752217dc5c03
Joseph Simpson <joe@radicalappdev.com>
2026-05-13 10:31:46 -0400

    Starting example 143

---
+++ Step Into Example Code/Examples/Example143.swift
@@ -0,0 +1,31 @@
+//  Step Into Vision - Example Code
+//
+//  Title: Example143
+//
+//  Subtitle: RealityKit Basics: MeshDescriptor
+//
+//  Description: Building shapes with MeshDescriptor
+//
+//  Type: Volume
+//
+//  Featured: true
+//
+//  Created by Joseph Simpson on 5/13/26.
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct Example143: View {
+    var body: some View {
+        RealityView { content in
+
+            
+
+        }
+    }
+}
+
+#Preview {
+    Example143()
+}
